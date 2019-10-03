dbea1420db4d534cdf868bc23538026563654835
Andreas Kraemer <a-kraemer@outlook.com>
2019-10-03 19:04:10 +0100

    added Character Sprites, Animations and Prefabs for Hero,NPC and Monsters 1-4 (Animations: Walking and Idle for Player, Idle for Monster4)

--- GameProject/Assets/Scripts/CRT_EffectCamera.cs
+++ GameProject/Assets/Scripts/CRT_EffectCamera.cs
@@ -6,28 +6,24 @@ public class CRT_EffectCamera : MonoBehaviour
 {
     public Material EffectMaterial;
     public bool m_CRTShaderOn=true;
-    public float scanlineCount=1000.0f;
-    public float scanlineIntesity=1f;
+    public float scanlineCount=500.0f;
+    public float scanlineBrightness=1f;
     public float vignetteStrenght=0.3f;
     private float time;
     public float scanlineSpeed=10.0f;
     public float noiseSize=5;
     public float noiseAmount=1f;
-    public float interlaceStrength=5;
+    public float flickerEffectStrenght=5;
 
-void Start()
-{
-    
-}
 void OnRenderImage(RenderTexture src,RenderTexture dst)
     {
         EffectMaterial.SetFloat("scanlineCount",scanlineCount);
-    EffectMaterial.SetFloat("scanlineIntensity",scanlineIntesity);
-    EffectMaterial.SetFloat("scanlineSpeed",scanlineSpeed);
-    EffectMaterial.SetFloat("vignetteStrenght",vignetteStrenght);
-    EffectMaterial.SetFloat("noiseSize",noiseSize);
-    EffectMaterial.SetFloat("noiseAmount",noiseAmount);
-    EffectMaterial.SetFloat("interlaceStrenght",interlaceStrength);
+        EffectMaterial.SetFloat("scanlineIntensity",scanlineBrightness);
+        EffectMaterial.SetFloat("scanlineSpeed",scanlineSpeed);
+        EffectMaterial.SetFloat("vignetteStrenght",vignetteStrenght);
+        EffectMaterial.SetFloat("noiseSize",noiseSize);
+        EffectMaterial.SetFloat("noiseAmount",noiseAmount);
+        EffectMaterial.SetFloat("interlaceStrenght",flickerEffectStrenght);
         EffectMaterial.SetFloat("time",Time.fixedTime);
         if(m_CRTShaderOn)Graphics.Blit(src,dst,EffectMaterial);
         else Graphics.Blit(src,dst);
