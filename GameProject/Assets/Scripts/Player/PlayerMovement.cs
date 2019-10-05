49a0e9128c44b5a8f1085431e61633492a3a7de1
lacia9419 <46899186+lacia9419@users.noreply.github.com>
2019-10-05 19:34:56 +0100

    Added Input Script
    Added input to work across all platforms. Also made updates to player movement script to make it more optimal.

--- GameProject/Assets/Scripts/Player/PlayerMovement.cs
+++ GameProject/Assets/Scripts/Player/PlayerMovement.cs
@@ -2,17 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * Player Movement Script
+ * 
+ * This script gets the input using the Unity Input manager for K/M and controller input, as well as touch data and outputs it using 3 public functions. This allows other scripts to access input data from across all platforms using one function.
+ * 
+ * Owner: 
+ * Last Edit : 
+ * 
+ * Also Edited by : Lewis Cleminson
+ * Last Edit: 05.10.19
+ * Reason: Update script to use the new Input Manager script to allow cross platform input.
+ * Also each Update the script was getting 6 components which is not optimal. Changed it to get components once at start and call on them, which uses less processing.
+ * 
+ * */
+
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 100;
     public GameObject menu;
+
+
+    //Start of Update from LC
+    private Rigidbody2D myRigid;
+    private Animator myAnim;
+    private SpriteRenderer myRenderer;
+    private InputManager IM;
+
+    private void Start()
+    {
+        myRigid = GetComponent<Rigidbody2D>();
+        myAnim = GetComponent<Animator>();
+        myRenderer = GetComponent<SpriteRenderer>();
+        IM = FindObjectOfType<InputManager>();
+    }
+
     void Update()
     {
-        GetComponent<Rigidbody2D>().velocity = new Vector2(Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed, Input.GetAxisRaw("Vertical") * Time.deltaTime * speed);
-        FlipSprite(GetComponent<Rigidbody2D>().velocity.x);
-        GetComponent<Animator>().SetFloat("SpeedX",Mathf.Abs(Input.GetAxisRaw("Horizontal")));
-        GetComponent<Animator>().SetFloat("SpeedY",Input.GetAxisRaw("Vertical"));
-        if (Input.GetButtonDown("Fire2"))
+       myRigid.velocity = new Vector2(IM.X_Axis(), IM.Y_Axis()) * Time.deltaTime * speed;
+       myRenderer.flipX = (myRigid.velocity.x < 0);
+       myAnim.SetFloat("SpeedX", Mathf.Abs(IM.X_Axis()));
+       myAnim.SetFloat("SpeedY",IM.Y_Axis());
+        if (IM.Button_Menu())
         {
             menu.SetActive(true);
             this.enabled = false;
@@ -21,11 +52,31 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnDisable()
     {
-        GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        myRigid.velocity = new Vector2(0, 0);
     }
-    private void FlipSprite(float velocityX)
-    {
-        if(velocityX<0)GetComponent<SpriteRenderer>().flipX=true;
-        else if(velocityX>0)GetComponent<SpriteRenderer>().flipX=false;
-    }
+
+    //End of update from LC
+
+    //void Update()
+    //{
+    //    GetComponent<Rigidbody2D>().velocity = new Vector2(Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed, Input.GetAxisRaw("Vertical") * Time.deltaTime * speed);
+    //    FlipSprite(GetComponent<Rigidbody2D>().velocity.x);
+    //    GetComponent<Animator>().SetFloat("SpeedX",Mathf.Abs(Input.GetAxisRaw("Horizontal")));
+    //    GetComponent<Animator>().SetFloat("SpeedY",Input.GetAxisRaw("Vertical"));
+    //    if (Input.GetButtonDown("Fire2"))
+    //    {
+    //        menu.SetActive(true);
+    //        this.enabled = false;
+    //    }
+    //}
+
+    //private void OnDisable()
+    //{
+    //    GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+    //}
+    //private void FlipSprite(float velocityX)
+    //{
+    //    if(velocityX<0)GetComponent<SpriteRenderer>().flipX=true;
+    //    else if(velocityX>0)GetComponent<SpriteRenderer>().flipX=false;
+    //}
 }
