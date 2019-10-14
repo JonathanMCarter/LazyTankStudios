bb9c30ac7d305d0d6dff2cdd8f097376625c5860
gabrielpotamianos <33257116+gabrielpotamianos@users.noreply.github.com>
2019-10-14 14:44:59 +0100

    Vendor Dialogue
    Script that assigns  dialogue file when triggered by interactable zone

---
+++ GameProject/Assets/Scripts/Shop/Vendor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vendor : MonoBehaviour
+{
+    /* 
+     * Vendor Script
+     * 
+     * Created by: Gabriel Potamianos
+     * Last edited: 14/10/2019
+     * 
+     * It does retrieve the file dialogue and connects it to the dialogue handler.
+     * 
+
+    */
+    //File to get Dialgoue
+    public DialogueFile VendorSpeech;
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Hero")
+            GameObject.Find("DialogueHandler").GetComponent<DialogueScript>().ChangeFile(VendorSpeech);
+
+    }
+}
