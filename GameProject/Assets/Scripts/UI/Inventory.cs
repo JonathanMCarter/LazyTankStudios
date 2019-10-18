5870e59d15d12c8c83d1d299aca63825bee72f2f
gabrielpotamianos <33257116+gabrielpotamianos@users.noreply.github.com>
2019-10-18 08:02:23 +0100

    Inventory Update
    Inventory is working but I still need to add money coditions and BUY/SELL panel

--- GameProject/Assets/Scripts/UI/Inventory.cs
+++ GameProject/Assets/Scripts/UI/Inventory.cs
@@ -13,7 +13,7 @@ using UnityEngine;
  * Class to handle inventory
  * Uses very simple method of storing the items as they are in fixed slots
  * 
- * Reason - Scales the inventory window into a smaller one while vendorMode active
+ * Reason - Scales the inventory window into a smaller one while vendorInventory active
  *        - Bug fixed: Menu activates player movement when it is closed before inventory
  *  
  */
@@ -22,21 +22,34 @@ public class Inventory : MonoBehaviour
 
     bool[] items;
     public int equipped = -1;
-
-    int selected = -1;
-    public bool isOpen;
     
+    public bool isOpen;
 
+
+    #region Gabriel Variables
 
     [HideInInspector]
-    public bool vendorMode;
     public const int ROWS = 3;
     public const int COLUMN = 4;
-    public int column=4;
+    public int selected = 0;
+    int column = 4;
+    public bool VendorMode { get; set; }
 
+    #endregion
+
+    #region Toby Functions
     //Add item to inventory with quantity, bool to remove as well can also use this to increase quantity
     public void addItem(int id, int quantity, bool remove)
     {
+        items[id] = !remove;
+        InvSlot slot = transform.GetChild(id).GetComponent<InvSlot>();
+        slot.hasItem = !remove;
+        slot.quantity = remove ? 0 : quantity;
+        slot.updateIcon();
+    }
+    public void addItem(int id, int quantity, bool remove,int QuantityToBeChangedWith,bool Increase)
+    {
+        quantity = Increase ? quantity += 1 : quantity -= 1;
         items[id] = !remove;
         InvSlot slot = transform.GetChild(id).GetComponent<InvSlot>();
         slot.hasItem = !remove;
@@ -70,14 +83,36 @@ public class Inventory : MonoBehaviour
     public void open()
     {
         isOpen = !isOpen;
-        selected = -1;
+        selected = 0;
     }
 
+    #endregion
+
+    #region Gabriel Functions
+
+    //Toogles the slots and changes the colour while doing that
+    public IEnumerator ToogleSlots(bool state)
+    {
+        yield return new WaitForSeconds(0.1f);
+        for (int i = 0; i < items.Length; i++)
+        {
+            transform.GetChild(i).GetComponent<InvSlot>().UnselectedColourApplied();
+            transform.GetChild(i).GetComponent<InvSlot>().enabled = state;
+
+        }
+    }
+
+    //Changes position of the inventory panel while in vendor mode
+    public void VendorPanelPosition(int value)
+    {
+        gameObject.transform.position = new Vector3(transform.position.x - value, transform.position.y, transform.position.z);
+    }
+
+    #endregion
     void Start()
     {
         items = new bool[transform.childCount];
-        vendorMode = gameObject.name.Equals("VendorInventory");
-        print(vendorMode);
+        VendorMode = false;
     }
 
     private bool delayed = false;
@@ -93,28 +128,30 @@ public class Inventory : MonoBehaviour
                 open();
                 StartCoroutine(delay());
             }
-            if (Input.GetButtonDown("Submit") && !vendorMode)
+            if (Input.GetButtonDown("Submit") && !VendorMode)
             {
                 equipItem(selected, !isEquipped(selected));
                 StartCoroutine(delay());
             }
 
+            #region Gabriel Edit Selector Movement
+
             //Lock selector in horizontal depending on which row it is (RIGHT END)
-            if (Input.GetAxisRaw("Horizontal") == 1 && !vendorMode)
+            if (Input.GetAxisRaw("Horizontal") == 1 )
             {
                 if (selected < column - 1) selected++;
                 StartCoroutine(delay());
             }
 
             //Lock selector on horizontal depending on which row it is (LEFT END)
-            else if (Input.GetAxisRaw("Horizontal") == -1 && !vendorMode)
+            else if (Input.GetAxisRaw("Horizontal") == -1 )
             {
                 if (selected > column - 1 - ROWS) selected--;
                 StartCoroutine(delay());
             }
 
             //Lock selector on vertical depending whether it exceeds the limits (LEFT END)
-            if (Input.GetAxisRaw("Vertical") == 1 && !vendorMode)
+            if (Input.GetAxisRaw("Vertical") == 1 )
             {
                 if (selected - COLUMN >= 0)
                 {
@@ -125,7 +162,7 @@ public class Inventory : MonoBehaviour
             }
 
             //Lock selector on vertical depending whether it exceeds the limits (RIGHT END )
-            else if (Input.GetAxisRaw("Vertical") == -1 && !vendorMode)
+            else if (Input.GetAxisRaw("Vertical") == -1)
             {
                 if (selected + COLUMN < items.Length)
                 {
@@ -134,6 +171,10 @@ public class Inventory : MonoBehaviour
                 }
                 StartCoroutine(delay());
             }
+
+            #endregion
+
+            //Emphasize the slot 
             for (int i = 0; i < items.Length; i++)
                 transform.GetChild(i).GetComponent<InvSlot>().selected = i == selected;
 
@@ -150,12 +191,7 @@ public class Inventory : MonoBehaviour
         delayed = false;
     }
 
-    //Changes position of the inventory panel while in vendor mode
-    public void ToogleVendorModeON(int value)
-    {
 
-        gameObject.transform.position = new Vector3(transform.position.x - value, transform.position.y, transform.position.z);
-    }
 
 
 
