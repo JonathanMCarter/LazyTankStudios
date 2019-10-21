b2779d55e5b9146f2268955f859aef8ea5bdc7e8
Jonathan Carter <2cartj31@solent.ac.uk>
2019-10-21 14:17:17 +0100

    Scene trans kinda working
    Changing scenes works but the player pos is not updated when you change scene yet.
    
    You'll need to not have:
    - Hero
    - Main Cam Prefab (from stuff that works)
    - GameManager
    - OptionsMenu
    -HealthUI

--- GameProject/Assets/Scripts/Level/InteriorScript.cs
+++ GameProject/Assets/Scripts/Level/InteriorScript.cs
@@ -1,24 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 /*
  * Created by Toby Wishart
  * Last edit: 11/10/19
+ * 
+ * Last edit: Jonathan @ 14:07 ish - 21/10/19
+ * added modes so the player can change scene
  */
 public class InteriorScript : MonoBehaviour
 {
+    public enum Modes
+    {
+        Internal,
+        Scene,
+    };
+
+    public Modes Type;
+
+    public string SceneName;
+
     public GameObject destination;
     [HideInInspector]
     public bool inside = false;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player") && !inside)
+        switch (Type)
         {
-            collision.transform.position = destination.transform.position;
-            destination.GetComponent<InteriorScript>().inside = true;
+            case Modes.Internal:
+                if (collision.CompareTag("Player") && !inside)
+                {
+                    collision.transform.position = destination.transform.position;
+                    destination.GetComponent<InteriorScript>().inside = true;
+                }
+                break;
+            case Modes.Scene:
+                if (SceneName.Length != 0)
+                {
+                    SceneManager.LoadScene(SceneName);
+                }
+                break;
+            default:
+                break;
         }
+
     }
 
     private void OnTriggerExit2D(Collider2D collision)
