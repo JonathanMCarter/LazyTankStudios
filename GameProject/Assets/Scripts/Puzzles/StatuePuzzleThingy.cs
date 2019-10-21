dc1a4de19d393462766622a48be54acba98c6d2d
Jonathan Carter <2cartj31@solent.ac.uk>
2019-10-21 10:40:38 +0100

    quests guy

---
+++ GameProject/Assets/Scripts/Puzzles/StatuePuzzleThingy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatuePuzzleThingy : MonoBehaviour
+{
+
+    public List<GameObject> Room1Em;
+    public List<GameObject> Room2Em;
+
+    public List<GameObject> RoomItems;
+
+    public PlayerMovement Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = FindObjectOfType<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Room1Cleared()
+    {
+        if (Room1Em.Count == 0)
+        {
+            GameObject Go = Instantiate(RoomItems[0], Player.gameObject.transform.position, Player.gameObject.transform.rotation);
+        }
+    }
+}
