5633614fe285de52ddce85f38c750346b7a92fbf
Jonathan Carter <2cartj31@solent.ac.uk>
2019-10-21 14:43:03 +0100

    Quest Added
    Added a quest where the player goes to a point, and thats it :), its just a simple thingy to show it working.

---
+++ GameProject/Assets/Scripts/Quests/QuestEndTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * 
+ * Made by jonathan as a test thingy to show the quests working...
+ * 21/10/19
+ * 
+ * 
+ * 
+ */ 
+public class QuestEndTest : MonoBehaviour
+{
+    public Quest QST;
+    public Quests QS;
+
+    public DialogueScript DS;
+    public DialogueFile File;
+
+
+    private void Start()
+    {
+        QS = FindObjectOfType<Quests>();
+        DS = FindObjectOfType<DialogueScript>();
+        QST = QS.Quest;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (QST.status == Quest.Status.OnGoing)
+        {
+            QST.status = Quest.Status.Completed;
+            DS.ChangeFile(File);
+            Debug.Log("quest done?");
+        }
+    }
+}
