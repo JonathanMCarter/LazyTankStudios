959efeed920f002dab100757881154519c57efd5
Andreas Kraemer <4kraea41@solent.ac.uk>
2019-10-22 16:24:46 +0100

    added cutscene assets and UI fixes

---
+++ GameProject/Assets/Scripts/Cutscenes/ScreenFade.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class ScreenFade : MonoBehaviour
+{
+
+    public Text titleText;
+    public Image mapImage;
+    public string loadLevel;
+
+    private IEnumerator Start()
+    {
+        titleText.canvasRenderer.SetAlpha(0.0f);
+        mapImage.canvasRenderer.SetAlpha(0.0f);
+
+        FadeIn();
+        yield return new WaitForSeconds(8f);
+        FadeOut();
+        yield return new WaitForSeconds(4f);
+        SceneManager.LoadScene(loadLevel);
+    } 
+    
+    void FadeIn()
+    {
+        titleText.CrossFadeAlpha(1.0f, 1.5f, false);
+        mapImage.CrossFadeAlpha(1.0f, 1.5f, false);
+    }
+
+    void FadeOut()
+    {
+        titleText.CrossFadeAlpha(0.0f, 2.5f, false);
+       mapImage.CrossFadeAlpha(0.0f, 2.5f, false);
+    }
+}
