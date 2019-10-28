c6155b8badf50afcab7ff458d21b223c9a7379eb
ghillman <ghillman@live.co.uk>
2019-10-28 18:51:20 +0000

    Part way through lite tiles
    Light tile puzzle still needs some work, however getting somewhere

---
+++ GameProject/Assets/Scripts/Puzzles/LightingUpPlatform/LightDoor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class LightDoor : MonoBehaviour
+{
+    public int LitTiles;
+    public int TotalTiles;
+    public BoxCollider2D Door;
+   // Update is called once per frame
+    void Update()
+    {
+        if(LitTiles >= TotalTiles)
+        {
+            Door.enabled = false;
+        }
+        else
+        {
+            Door.enabled = true;
+        }
+    }
+}
---
+++ GameProject/Assets/Scripts/Puzzles/LightingUpPlatform/TileLight.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class TileLight : MonoBehaviour
+{
+    private bool Lit;
+    public LightDoor Door;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if(Lit)
+            {
+                Door.LitTiles++;
+            }
+            else
+            {
+                Door.LitTiles--;
+            }
+        }
+    }
+}
