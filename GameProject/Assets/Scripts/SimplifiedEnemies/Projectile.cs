642afcd714180cae4c3ca2ed99a784ab4ecb5fbd
TomLast <tomlast98@outlook.com>
2019-11-05 11:26:53 +0000

    e

---
+++ GameProject/Assets/Scripts/SimplifiedEnemies/Projectile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Final
+{
+    public class Projectile : Ability
+    {
+        public GameObject projectilePrefab;
+        public Vector2 colliderSize = Vector2.one * 0.5f;
+        public float ProjectileSpeed;
+        public float ProjectileRange;
+        public Sprite ProjectileSprite;
+        public override IEnumerator Use()
+        {
+            BasicAttackCollider bac = Instantiate(projectilePrefab).GetComponent<BasicAttackCollider>();
+            bac.Init(transform.position, colliderSize, ProjectileSpeed, ProjectileRange, (Player.Value.transform.position - transform.position).normalized, ProjectileSprite);
+            yield return null;
+        }
+    }
+}
