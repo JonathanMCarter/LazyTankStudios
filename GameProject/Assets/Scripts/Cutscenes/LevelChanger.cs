8f3f6a5e8c5e80f65777172a4e2341318d161d7f
VampireViktor20 <kylecornw3ll@outlook.com>
2019-11-12 01:29:46 +0000

    Intro Cinematic Complete
    - Implemented Inro Cutscene into game.
    - Outlines story and world.
    - New cutscene files created.
    - Must be tested for mobile build for layout purposes.

---
+++ GameProject/Assets/Scripts/Cutscenes/LevelChanger.cs
@@ -0,0 +1,29 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour
+{
+
+    public Animator animator;
+
+    public int levelToLoad;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            FadeToLevel(levelToLoad);
+        }
+
+    }
+    public void FadeToLevel (int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        animator.SetTrigger("FadeOut");
+    }
+    public void OnFadeComplete ()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
