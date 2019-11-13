95c9f444ae77b1c21d19500affdef991b28df731
Toby <qwenit56@gmail.com>
2019-11-13 22:50:03 +0000

    Black lines should be fixed now

--- GameProject/Assets/Scripts/Camera/CameraMove.cs
+++ GameProject/Assets/Scripts/Camera/CameraMove.cs
@@ -3,29 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 /*
  * Created by Toby Wishart
- * Last edit: 03/11/19
- * Reason: Camera.main accesses the mobile camera and not the main camera
+ * Last edit: 13/11/19
+ * Reason: Fixing tears in the tilemap
  */
 public class CameraMove : MonoBehaviour
 {
 
     private Transform p;
+    Camera cam;
 
     void Start()
     {
         p = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        cam = GetComponent<Camera>();
     }
 
     void Update()
     {
+        if (cam.pixelHeight % 2 != 0) Debug.Log("Screen height is an odd number, expect the black lines");
+        float scale = (8.0F/*The PPU of the assets*/ * Mathf.Max(1, Mathf.Min(((float)Screen.height / 108.0F/*The target height*/), ((float)Screen.width / 192.0F/*The target width*/))));
+        cam.orthographicSize = ((float)Screen.height / 2F) / scale;
         if (GameObject.Find("Boundary"))
         {
             BoxCollider2D b = GameObject.Find("Boundary").GetComponent<BoxCollider2D>();
-            BoxCollider2D cam = GetComponent<BoxCollider2D>();
+            BoxCollider2D camBox = GetComponent<BoxCollider2D>();
             //Adjust the camera box collider based on the size of the camera
-            cam.size = new Vector2(GetComponent<Camera>().orthographicSize * 2 * GetComponent<Camera>().aspect, GetComponent<Camera>().orthographicSize * 2);
+            camBox.size = new Vector2(cam.orthographicSize * 2 * cam.aspect, cam.orthographicSize * 2);
             //Follow the player and stay within the boundaries
-            transform.position = new Vector3(Mathf.Clamp(p.position.x, b.bounds.min.x + cam.size.x / 2, b.bounds.max.x - cam.size.x / 2), Mathf.Clamp(p.position.y, b.bounds.min.y + cam.size.y / 2, b.bounds.max.y - cam.size.y / 2), transform.position.z);
+            transform.position = new Vector3(Mathf.Clamp(p.position.x, b.bounds.min.x + camBox.size.x / 2, b.bounds.max.x - camBox.size.x / 2), Mathf.Clamp(p.position.y, b.bounds.min.y + camBox.size.y / 2, b.bounds.max.y - camBox.size.y / 2), transform.position.z);
         }
+        //Units per pixel
+        float upp = 1.0F / scale;
+        //Snap camera movement to pixels
+        transform.position = new Vector3(Mathf.Round(transform.position.x / upp) * upp, Mathf.Round(transform.position.y / upp) * upp, transform.position.z);
     }
 }
