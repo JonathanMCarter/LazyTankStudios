e6943965d7cee80e8ef4f2691ba01f411a8a9467
Andreas Kraemer <a-kraemer@outlook.com>
2019-11-18 11:39:22 +0000

    removed commented out parts from camera folder

--- GameProject/Assets/Scripts/Camera/ZoneTransition.cs
+++ GameProject/Assets/Scripts/Camera/ZoneTransition.cs
@@ -11,48 +11,14 @@ using UnityEngine.SceneManagement;
  */
 public class ZoneTransition : A
 {
-
-    //Animator a;
     public string destination;
-   // GameObject player;
-
-    void Start()
-    {
-        //a = GameObject.Find("ZoneFadeScreen").GetComponent<Animator>();
-       // player = GameObject.FindWithTag("Player");
-
-    }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            //SceneManager.LoadScene(destination);
-
              FindObjectOfType<PlayerZoneTransition>().StartTransition(destination);
-            //player.GetComponent<PlayerMovement>().enabled = false;
-            //player.GetComponent<PlayerZoneTransition>().Destination = destination;
-            //Camera.main does not work here
-            //GameObject.Find("Main Camera").GetComponent<CameraMove>().enabled = false;
-            //a.SetBool("in", false);
-            //a.SetBool("out", true);
         }
     }
-
-    //Function for fade out animation event
-    //public void FadeOutEnd()
-    //{
-
-
-    //}
-
-    ////Function for fade in animation event
-    //public void FadeInEnd()
-    //{
-    //    //a.SetBool("in", false);
-    //    //a.SetBool("out", false);
-    //    //player.GetComponent<PlayerMovement>().enabled = true;
-
-    //}
 
 }
