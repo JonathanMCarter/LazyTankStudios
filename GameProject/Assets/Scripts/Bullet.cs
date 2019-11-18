cd3c3441d30f5c22e8b7696c9652b66c051f94b8
Mitchell Dixon <mitchelldixon35@gmail.com>
2019-11-18 14:50:03 +0000

    Update Bullet.cs
    optimise

--- GameProject/Assets/Scripts/Bullet.cs
+++ GameProject/Assets/Scripts/Bullet.cs
@@ -1,50 +1,31 @@
 ﻿using UnityEngine;
 
-
-
-/*
- * Created by: Lewis Cleminson
- * Last Edit: 21.10.19
- * 
- * Didnt want to create this script. Currently storing bullet speed and damage information
- * 
- * Also edited by: Toby Wishart
- * Last edit: 11/10/19
- * Reason: Store source item and allow for unlimited lifespan for sword
- * 
- * */
-
-
 public class Bullet : A
 {
-    public int Damage;
-    public Vector2 Speed;
-    public float Lifespan;
-    //The ID of the item used to spawn projectile set to -1 or any other invalid ID for no source
+    public int Dmg;
+    public Vector2 Sp;
+    public float Lspan;
     public int SourceItem = -1;
 
-    public void SetStats(int damage, Vector2 speed, float lifespan, int sourceItem)
+    public void SetStats(int dmg, Vector2 speed, float lspan, int sourceItem)
     {
-        Damage = damage;
-        Speed = speed;
-        Lifespan = lifespan;
+        Dmg = dmg;
+        Sp = speed;
+        Lspan = lspan;
         SourceItem = sourceItem;
 
-        GetComponent<Rigidbody2D>().AddForce(Speed, ForceMode2D.Impulse);
-        //Allow for unlimited lifespan
-        if (Lifespan >= 0) Destroy(gameObject, Lifespan);
+        GetComponent<Rigidbody2D>().AddForce(Sp, ForceMode2D.Impulse);
+        if (Lspan >= 0) Destroy(gameObject, Lspan);
     }
-
-
 
     public void IncreaseSpeed()
     {
-        GetComponent<Rigidbody2D>().AddForce(Speed, ForceMode2D.Impulse);
-        Speed = Speed * 2;
+        GetComponent<Rigidbody2D>().AddForce(Sp, ForceMode2D.Impulse);
+        Sp = Sp * 2;
     }
 
-    public void IncreaseDamage(int damageChange)
+    public void IncreaseDamage(int dmgChange)
     {
-        Damage = Damage * damageChange;
+        Dmg = Dmg * dmgChange;
     }
 }
