1df7a6eec2f8ea5c857cbd23adc536ae37c2e962
Jonathan Carter <2cartj31@solent.ac.uk>
2019-11-19 12:50:41 +0000

    Item Scripts opt

--- GameProject/Assets/Scripts/Items/ItemCheck.cs
+++ GameProject/Assets/Scripts/Items/ItemCheck.cs
@@ -1,12 +1,1 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ItemCheck : A {
-
-    public int item;
-    void Update()
-    {
-        if (GameObject.FindGameObjectWithTag("Inv").GetComponent<Inventory>().hasItem(item)) gameObject.SetActive(false);
-    }
-}
+﻿using UnityEngine;public class ItemCheck : A {public int item;void Update(){if (GameObject.FindGameObjectWithTag("Inv").GetComponent<Inventory>().hasItem(item)) gameObject.SetActive(false);}}
