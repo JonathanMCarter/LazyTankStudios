651fa1e6d1982b50df37e894a12f6174b82def82
gabrielpotamianos <33257116+gabrielpotamianos@users.noreply.github.com>
2019-11-23 14:23:59 +0000

    Vendor System Working
    Vendor System is now able to provide potions only. You can only aquire them if you have enough currency.
    
    For testing purposes I added a feature which provides you 50 gold every time you press Enter Button.
    
    Note: To be removed from Vendor.cs - Update function

--- GameProject/Assets/Scripts/Shop/Vendor.cs
+++ GameProject/Assets/Scripts/Shop/Vendor.cs
@@ -1,97 +1,40 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-/*public class Vendor: A {
- public DialogueFile VendorSpeech;
- Inventory Pinv, VInv;
- InvSlot pInvSlot, VInvSlot;
- bool happened;
- bool Sell;
- bool active;
- DialogueScript DS;
- bool IsUsingVendor;
- SoundPlayer audioManager;
- InputManager IM;
- public List < int > itemsSold;
- void Awake() {
-  audioManager = FindObjectOfType < SoundPlayer > ();
-  IM = FindObjectOfType < InputManager > ();
-  Inventory[] i = GameObject.Find("SellOrBuyPanel").GetComponentsInChildren < Inventory > ();
-  Pinv = i[0];
-  VInv = i[1];
-  DS = FindObjectOfType < DialogueScript > ();
- }
- bool delayed;
- IEnumerator delay() {
-  delayed = true;
-  yield
-  return new WaitForSeconds(0.2f);
-  delayed = false;
- }
- void Update() {
-  if (active) {
-   pInvSlot = Pinv.Slots[Pinv.selected];
-   VInvSlot = VInv.Slots[VInv.selected];
-   if (IsUsingVendor) {
-    if (IM.Button_B() && !delayed) {
-     Sell = !Sell;
-     StartCoroutine(delay());
+﻿using UnityEngine;
+using UnityEditor;
+public class Vendor: A {
+
+    public GameObject p;
+    public int price;
+    bool happened = false;
+    CanvasGroup cg;
+    InputManager IM;
+    Inventory inv;
+    private void Start()
+    {
+        cg = G<CanvasGroup>(F("VendorInventory"));
+        IM = F<InputManager>();
+        inv = G<Inventory>(F("PlayerInventory"));
     }
-    if (IM.Button_Menu()) {
-     IsUsingVendor = false;
-     active = false;
-     Sell = true;
+    private void Update()
+    {
+        if (F<DialogueScript>().FileHasEnded && !happened)
+        {
+            cg.alpha = 1;
+            happened = !happened;
+        }
+        else if (happened && Input.GetKeyDown(KeyCode.Escape))
+            cg.alpha = 0;
+        G<PlayerMovement>(F("Hero")).enabled = cg.alpha == 1 ? false : true;
+        if (IM.Button_A() && cg.alpha == 1 && inv.getCoins() >= price)
+        {
+            p.GetComponent<InventoryItem>().pickup();
+            inv.addCoins(-price);
+        }
+        if (Input.GetKeyDown(KeyCode.Return))
+            inv.addCoins(50);
+        
     }
-    if (IM.Button_A()) {
-     if (pInvSlot.hasItem && Sell) {
-      Pinv.addItem(Pinv.selected, pInvSlot.quantity, pInvSlot.quantity <= 1, !Sell);
-      VInv.addItem(Pinv.selected, VInv.Slots[Pinv.selected].quantity, !Sell, Sell);
-      Pinv.addCoins(pInvSlot.Value);
-      audioManager.Play("Sell");
-     } else if (VInvSlot.hasItem && !Sell) {
-      if (Pinv.getCoins() >= VInvSlot.Value) {
-       Pinv.addItem(VInv.selected, Pinv.Slots[VInv.selected].quantity, Sell, !Sell);
-       VInv.addItem(VInv.selected, VInvSlot.quantity, VInvSlot.quantity <= 1, Sell);
-       Pinv.addCoins(-VInvSlot.Value);
-       audioManager.Play("Buy");
-      }
-     }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        happened = false;
     }
-   }
-   FindObjectOfType < PlayerMovement > ().enabled = !active;
-   Pinv.active = Sell;
-   VInv.active = !Sell;
-   if (DS.File == VendorSpeech) {
-    if (DS.FileHasEnded && !happened) {
-     IsUsingVendor = true;
-     Pinv.open();
-     Pinv.VendorMode = true;
-     VInv.open();
-     VInv.VendorMode = true;
-     happened = !happened;
-    }
-   }
-  }
- }
- bool exited = true;
- void OnTriggerEnter2D(Collider2D collision) {
-  if (collision.gameObject.name == "Hero") {
-   if (exited) {
-    exited = false;
-    happened = false;
-    active = true;
-    for (int i = 0; i < VInv.items.Length; i++) {
-     VInv.items[i] = false;
-     VInv.Slots[i].hasItem = false;
-     VInv.Slots[i].quantity = 0;
-     VInv.Slots[i].updateIcon();
-    }
-    itemsSold.ForEach(i => VInv.addItem(i, 98, false, true));
-   }
-  }
- }
- void OnTriggerExit2D(Collider2D collision) {
-  if (collision.gameObject.name == "Hero") exited = true;
- }
 }
-*/
