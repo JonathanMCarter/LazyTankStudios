4e8a5b18284de3a95000b5af572f2e2d152e40eb
Jonathan <jmc@devj.uk>
2019-11-24 13:27:16 +0000

    Update DialogueScript.cs
    Renamed some private or internal variables to reduce their file size a tad.

--- GameProject/Assets/Scripts/Dialogue/DialogueScript.cs
+++ GameProject/Assets/Scripts/Dialogue/DialogueScript.cs
@@ -3,58 +3,58 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 public class DialogueScript: A {
- public bool InCinematic = false;
+ bool InC = false;
  public DialogueFile File;
  public Text DialName;
  public Text DialText;
- int DialStage = 0;
- bool IsCoRunning;
+ int DS = 0;
+ bool IsCoR;
  public bool InputPressed;
  public bool RequireInput = true;
  public bool FileHasEnded = false;
  public int TypeWriterCount = 1;
  public int TypeWriterCharactersToAdvanceBy = 1;
- Coroutine PauseCo;
+ Coroutine PCo;
  public Animator AnimToPlay;
  void Update() {
   if (RequireInput) {
-   if ((!IsCoRunning) && (InputPressed) && !FileHasEnded) {
-    DisplayNextLine();
+   if ((!IsCoR) && (InputPressed) && !FileHasEnded) {
+    DNL();
    }
   }
  }
- public void ChangeFile(DialogueFile Input) {
-  if (Input) {
-   File = Input;
+ public void ChangeFile(DialogueFile I) {
+  if (I) {
+   File = I;
   }
-  Reset();
+  R();
  }
- public void DisplayNextLine() {
-  if (DialStage < File.Names.Count) {
-   switch (File.Names[DialStage]) {
+ public void DNL() {
+  if (DS < File.Names.Count) {
+   switch (File.Names[DS]) {
     case "###":
      DialName.text = "";
      DialText.text = "";
-     if (PauseCo == null) {
-      PauseCo = SC(CinematicLoad(File.Dialogue[DialStage]));
+     if (PCo == null) {
+      PCo = SC(CL(File.Dialogue[DS]));
      }
      break;
     case "@@@":
-     if (PauseCo == null) {
-      PauseCo = SC(PauseDial(3));
+     if (PCo == null) {
+      PCo = SC(PD(3));
      }
      break;
     case "^^^":
-     AnimToPlay.Play(File.Dialogue[DialStage], -1);
+     AnimToPlay.Play(File.Dialogue[DS], -1);
      break;
     case "***":
-     DialStage = 0;
+     DS = 0;
      FileHasEnded = true;
      break;
     default:
-     DialName.text = File.Names[DialStage];
-     DialText.text = File.Dialogue[DialStage];
-     DialStage++;
+     DialName.text = File.Names[DS];
+     DialText.text = File.Dialogue[DS];
+     DS++;
      InputPressed = false;
      break;
    }
@@ -69,34 +69,31 @@ public class DialogueScript: A {
    InputPressed = true;
   }
  }
- public void Reset() {
+ void R() {
   if (InputPressed) {
    InputPressed = false;
   }
   if (FileHasEnded) {
    FileHasEnded = false;
   }
-  DialStage = 0;
+  DS = 0;
  }
- IEnumerator PauseDial(float Delay) {
-  yield
-  return new WaitForSeconds(Delay);
-  ++DialStage;
+ IEnumerator PD(float D) {
+  yield return new WaitForSeconds(D);
+  ++DS;
  }
-IEnumerator CinematicFinish(string cinematic) {
-  InCinematic = true;
-  Scene s = SceneManager.GetSceneByName(cinematic);
-  yield
-  return new WaitWhile(() => s.isLoaded);
-  InCinematic = false;
-  DialStage++;
-  PauseCo = null;
+IEnumerator CF(string c) {
+  InC = true;
+  Scene s = SceneManager.GetSceneByName(c);
+  yield return new WaitWhile(() => s.isLoaded);
+  InC = false;
+  DS++;
+  PCo = null;
  }
-IEnumerator CinematicLoad(string cinematic) {
-  SceneManager.LoadSceneAsync(cinematic, LoadSceneMode.Additive);
-  Scene s = SceneManager.GetSceneByName(cinematic);
-  yield
-  return new WaitWhile(() => !s.isLoaded);
-  PauseCo = SC(CinematicFinish(cinematic));
+IEnumerator CL(string c) {
+  SceneManager.LoadSceneAsync(c, LoadSceneMode.Additive);
+  Scene s = SceneManager.GetSceneByName(c);
+  yield return new WaitWhile(() => !s.isLoaded);
+  PCo = SC(CF(c));
  }
 }
