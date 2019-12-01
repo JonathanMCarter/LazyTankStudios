0498f192f38fac722e529f7da799479c22a96955
Gamecrazy1 <33253643+Ganecrazy1@users.noreply.github.com>
2019-12-01 13:42:31 +0000

    Fire Trap Restored
    -Restored the fire.cs script and the fire trap prefab from the last pre-cuts build.

---
+++ GameProject/Assets/Scripts/Puzzles/FireTrap.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireTrap : A
+{
+    public int Damage;
+    public float ActiveSeconds, WaitSeconds, damageTime;
+    public Sprite ActiveSprite, WaitSprite;
+    float Timer, damageTimer;
+    bool Active, DamageTrigger;
+    BoxCollider2D MyCol;
+    SpriteRenderer MyRend;
+    PlayerMovement MyPlay;
+    void Start()
+    {
+        Timer = 0f;
+        Active = false;
+        MyCol = GetComponent<BoxCollider2D>();
+        MyRend = GetComponent<SpriteRenderer>();
+        MyPlay = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
+    }
+    void Update()
+    {
+        Timer += Time.deltaTime;
+        if (!Active)
+        {
+            if (Timer >= ActiveSeconds)
+            {
+                Active = true;
+                Timer = 0f;
+                MyCol.enabled = true;
+                MyRend.sprite = ActiveSprite;
+            }
+        }
+        else
+        {
+            if (Timer >= WaitSeconds)
+            {
+                Active = false;
+                Timer = 0f;
+                MyCol.enabled = false;
+                MyRend.sprite = WaitSprite;
+            }
+        }
+        if (DamageTrigger)
+        {
+            damageTimer += Time.deltaTime;
+            if (damageTimer >= damageTime)
+            {
+                MyPlay.TakeDamage(Damage);
+            }
+        }
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            DamageTrigger = true;
+            MyPlay.TakeDamage(Damage);
+        }
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            DamageTrigger = false;
+        }
+    }
+}
