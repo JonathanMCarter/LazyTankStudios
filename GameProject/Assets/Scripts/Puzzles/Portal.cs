e8029df53f149efd6f33287fca40ccce539afb1e
lacia9419 <46899186+lacia9419@users.noreply.github.com>
2019-12-05 03:41:51 +0000

    dungeons tweak
    fixed AI being teleported by puzzle, snow stronghold boss can now be killed.

--- GameProject/Assets/Scripts/Puzzles/Portal.cs
+++ GameProject/Assets/Scripts/Puzzles/Portal.cs
@@ -3,6 +3,7 @@ public class Portal: A {
  public GameObject portal;
  public float XOffset, YOffset;
  void OnTriggerStay2D(Collider2D collision) {
-  collision.transform.position = new Vector2(portal.transform.position.x + XOffset, portal.transform.position.y + YOffset);
+  if (collision.gameObject.name == "Hero") collision.transform.position = new Vector2(portal.transform.position.x + XOffset, portal.transform.position.y + YOffset);
+  //LC added check to only move the player
  }
 }
