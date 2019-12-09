8a9522327c98992224fdeb06f36ae2a615cff780
gabrielpotamianos <33257116+gabrielpotamianos@users.noreply.github.com>
2019-12-09 11:11:47 +0000

    Items OverScenes Fix Part 2

--- GameProject/Assets/Scripts/Camera/ZoneTransition.cs
+++ GameProject/Assets/Scripts/Camera/ZoneTransition.cs
@@ -3,7 +3,9 @@ public class ZoneTransition: A {
  public string destination;
  void OnTriggerEnter2D(Collider2D collision) {
   if (collision.gameObject.CompareTag("Player")) {
-   F<PlayerMovement>().load(destination.Split(':'));
+            GameObject.FindObjectOfType<SaveSys>().Save();
+
+            F<PlayerMovement>().load(destination.Split(':'));
   }
  }
 }
